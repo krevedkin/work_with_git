05ddcafa514668c7ed5fa3709499d7418654ffc5
krevedkin <frakestolstm@gmail.com>
2021-03-11 23:51:35 +0300

    added new vars and prints

--- script.py
+++ script.py
@@ -3,6 +3,11 @@ print("Hello")
 var_1 = "var"
 var_2 = "var"
 
+new = "hello"
+new_2 = "world"
+
 if __name__ == "__main__":
     if var_1 == var_2:
         print("True")
+
+    print(f"{new}{new_2}")
